f67b301b945060236e609c7f9e3d21c260eba25c
Tarps240 <sl3966@gmail.com>
2024-09-16 04:55:36 -0700

    halfway done commit.

---
+++ Assets/JS/main.js
@@ -0,0 +1,7 @@
+// Mobile Menu
+
+const mobileMenu = document.querySelector('.mobile-menu');
+
+mobileMenu.addEventListener('click', () => {
+    document.querySelector('.menu').classList.toggle('show')
+});
